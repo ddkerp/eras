7f23b0144c64dfd411c9484695c8668564ccdf78
Dilipkumar Durairajan <ddkerp1@gmail.com>
2016-08-03 18:36:05 +0530

    Update DocReader.php

--- library/DocReader.php
+++ library/DocReader.php
@@ -53,49 +53,7 @@
         return $striped_content;
     }
 
- /************************excel sheet************************************/
-
-function xlsx_to_text($input_file){
-    $xml_filename = "xl/sharedStrings.xml"; //content file name
-    $zip_handle = new ZipArchive;
-    $output_text = "";
-    if(true === $zip_handle->open($input_file)){
-        if(($xml_index = $zip_handle->locateName($xml_filename)) !== false){
-            $xml_datas = $zip_handle->getFromIndex($xml_index);
-            $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
-            $output_text = strip_tags($xml_handle->saveXML());
-        }else{
-            $output_text .="";
-        }
-        $zip_handle->close();
-    }else{
-    $output_text .="";
-    }
-    return $output_text;
-}
-
-/*************************power point files*****************************/
-function pptx_to_text($input_file){
-    $zip_handle = new ZipArchive;
-    $output_text = "";
-    if(true === $zip_handle->open($input_file)){
-        $slide_number = 1; //loop through slide files
-        while(($xml_index = $zip_handle->locateName("ppt/slides/slide".$slide_number.".xml")) !== false){
-            $xml_datas = $zip_handle->getFromIndex($xml_index);
-            $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
-            $output_text .= strip_tags($xml_handle->saveXML());
-            $slide_number++;
-        }
-        if($slide_number == 1){
-            $output_text .="";
-        }
-        $zip_handle->close();
-    }else{
-    $output_text .="";
-    }
-    return $output_text;
-}
-
+ 
 
     public function convertToText() {
 
@@ -108,7 +66,7 @@ function pptx_to_text($input_file){
         if($file_ext == "doc" || $file_ext == "docx" || $file_ext == "xlsx" || $file_ext == "pptx")
         {
             if($file_ext == "doc") {
-                return $this->read_doc();
+                return $this->getDocToText();
             } elseif($file_ext == "docx") {
                 return $this->read_docx();
             } elseif($file_ext == "xlsx") {
@@ -120,6 +78,26 @@ function pptx_to_text($input_file){
             return "Invalid File Type";
         }
     }
+    
+    public function getDocToText() {
+		if(file_exists($this->filename)) {
+			if(($fh = fopen($this->filename, 'r')) !== false ) {
+				$headers = fread($fh, 0xA00);
+				$n1 = ( ord($headers[0x21C]) - 1 );
+				$n2 = ( ( ord($headers[0x21D]) - 8 ) * 256 );
+				$n3 = ( ( ord($headers[0x21E]) * 256 ) * 256 );
+				$n4 = ( ( ( ord($headers[0x21F]) * 256 ) * 256 ) * 256 );
+				$textLength = ($n1 + $n2 + $n3 + $n4);
+				$extracted_plaintext = fread($fh, $textLength);
+				$extracted_plaintext = mb_convert_encoding($extracted_plaintext,'UTF-8');
+				 return ($extracted_plaintext);
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}  
+	}
 
 }
 ?>
